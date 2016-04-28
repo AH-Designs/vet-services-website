3fe2e979f3921ae81cde5c47f12d96d9f843c818
ah-designs <ahdesigns@outlook.com>
2016-04-28 19:07:24 +0100

    -aFirst commit for vet-services-website project

---
+++ Javascript/script.js
@@ -0,0 +1,15 @@
+
+//animsition plugin
+
+    $('.animsition').animsition({
+
+    inClass: 'zoom-in',
+    outClass: 'zoom-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link'
+
+
+
+    });
+
